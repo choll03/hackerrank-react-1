ba751853c83bbf6e78d5b4a72b4c1926934e4a3c
mail <ismail15000000@gmail.com>
2020-07-27 13:26:53 +0700

    update

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -2,30 +2,16 @@ import React, { useState } from 'react';
 
 function Slides({slides}) {
 
-    const [slideActive, setSildeActive] = useState(0);
-
-    const handleReset = () => {
-        setSildeActive(0);
-    }
-
-    const handleNext = () => {
-        setSildeActive((slideActive+1));
-    }
-
-    const handlePrev = () => {
-        setSildeActive((slideActive-1));
-    }
-
     return (
         <div>
             <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined" onClick={handleReset} disabled={slideActive === 0}>Restart</button>
-                <button data-testid="button-prev" className="small" onClick={handlePrev} disabled={slideActive === 0}>Prev</button>
-                <button data-testid="button-next" className="small" onClick={handleNext} disabled={(slideActive+1) === slides.length}>Next</button>
+                <button data-testid="button-restart" className="small outlined">Restart</button>
+                <button data-testid="button-prev" className="small">Prev</button>
+                <button data-testid="button-next" className="small">Next</button>
             </div>
             <div id="slide" className="card text-center">
-                <h1 data-testid="title">{slides[slideActive].title}</h1>
-                <p data-testid="text">{slides[slideActive].text}</p>
+                <h1 data-testid="title">Title</h1>
+                <p data-testid="text">Text</p>
             </div>
         </div>
     );
